bd00a9fe3551653efe30da6d2edd5c8086dab10e
Jamie <rjamie.leask@gmail.com>
2018-01-04 14:56:51 -0500

    added stuff

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,26 +1,38 @@
-// team1
-$(".game-page-wrapper").on("click", ".team1", () => {
-  $(".team1")
-    .parent()
-    .slideUp(500, () => {});
+$(document).ready(() => {
+  // $(".game-page-wrapper").append(
+  //   "<button class='btn btn-default roster2'>Roster</button>"
+  // );
+  // team1
+  $(".game-page-wrapper").on("click", ".team1", () => {
+    $(".team1")
+      .parent()
+      .addClass("disappear")
+      .removeClass("reappear");
+  });
+  $(".game-page-wrapper").on("click", ".roster", () => {
+    let nav = $(".navbar");
+    $(".roster")
+      .parent()
+      .find(nav)
+      .addClass("reappear")
+      .removeClass("disappear");
+  });
+  // $(".card-box").on("click", () => {
+  //   $(this).remove();
+  // });
+  // team 2
+  // $(".game-page-wrapper").on("click", ".team2", () => {
+  //   $(".team2")
+  //     .parent()
+  //     .addClass("disappear")
+  //     .removeClass("reappear");
+  // });
+  // $(".game-page-wrapper").on("click", ".roster2", () => {
+  //   let nav = $(".nav-bot");
+  //   $(".roster2")
+  //     .parent()
+  //     .find(nav)
+  //     .addClass("reappear")
+  //     .removeClass("disappear");
+  // });
 });
-$(".game-page-wrapper").on("click", ".roster", () => {
-  let nav = $("nav");
-  $(".roster")
-    .parent()
-    .find(nav)
-    .slideDown(500, "linear", () => {});
-});
-// team 2
-$(".nav-bot").on("click", ".team2", () => {
-  $(this)
-    .parent()
-    .slideDown(500, () => {});
-});
-// $(".game-page-wrapper").on("click", ".roster2", () => {
-//   let nav = $(".nav-bot");
-//   $(".roster2")
-//     .parent()
-//     .find(nav)
-//     .slideUp(500, "linear", () => {});
-// });
